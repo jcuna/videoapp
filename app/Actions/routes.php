f8c3ef573f30c8a086e1178fefa3db1a03eb5c7a
jcuna <garciajon@me.com>
2017-06-21 22:32:10 -0400

    finished bootstrapping and fixing a few bugs with framebro develop branch which is currently being use for it`s php7 signature

--- app/Actions/routes.php
+++ app/Actions/routes.php
@@ -14,44 +14,42 @@
  */
 namespace App\Core\Http\Routing {
 
-    use App\Core\Http\View;
+    use \App\Core\Http\View;
     use \App\Services\IsAuthenticated;
 
-    Router::all('login', 'Users@login', ['via' => 'login_path']);
+    Router::group(["prefix" => "api/v1"], function () {
 
-    Router::group(["before" => IsAuthenticated::class], function () {
-        Router::get('/', 'Home@index');
-        Router::get('users/logout', 'Users@logout');
-        Router::all('users/create', 'Users@create');
-        Router::all('users', 'Users@index');
-        Router::get('users/all', 'Users@allUsers');
-        Router::get('users/{username}', 'Users@index');
-        Router::post('users/login', 'Users@login');
-        Router::all('users/deleteCurrentUser', 'Users@deleteCurrentUser');
-    });
-//Example with closure
-    Router::get('testing/{name}', function ($name) {
-        echo("hey there $name");
-    });
+        Router::all('login', 'Users@login', ['via' => 'login_path']);
 
+        Router::group(["before" => IsAuthenticated::class], function () {
+            Router::get('/', 'Home@index');
+            Router::get('users/logout', 'Users@logout');
+            Router::all('users/create', 'Users@create');
+            Router::all('users', 'Users@index');
+            Router::get('users/all', 'Users@allUsers');
+            Router::get('users/{username}', 'Users@index');
+            Router::post('users/login', 'Users@login');
+            Router::all('users/deleteCurrentUser', 'Users@deleteCurrentUser');
+        });
 
-    Router::group(["before" => IsAuthenticated::class], function () {
-        // Example declaring multiple actions for a controller.
-        // This is also a useful admin menu that gives you info about the system and allows you to create the first user
-        Router::resources('admin', 'Admin',
-            [
-                ['get' =>
-                    ['index', 'showRoutes', 'logs', 'info', 'statusReport', 'memcachedStats', 'firstUser']
-                ],
-                ['post' =>
-                    ['index', 'firstUser']
+        Router::group(["before" => IsAuthenticated::class], function () {
+            // Example declaring multiple actions for a controller.
+            // This is also a useful admin menu that gives you info about the system and allows you to create the first user
+            Router::resources('admin', 'Admin',
+                [
+                    ['get' =>
+                        ['index', 'showRoutes', 'logs', 'info', 'statusReport', 'memcachedStats', 'firstUser']
+                    ],
+                    ['post' =>
+                        ['index', 'firstUser']
+                    ]
                 ]
-            ]
-        );
-    });
+            );
+        });
 
 //When a page is missing.
-    Router::missing(function () {
-        return View::render('errors/error', "The requested page doesn't exist", 404);
+        Router::missing(function () {
+            return View::render('errors/error', "The requested page doesn't exist", 404);
+        });
     });
 }
